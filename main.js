dc8c6ba2950e65382c27ee87e881cbb7dc6e29ef
Chester ONeill <c.oneill.codes@gmail.com>
2013-12-09 22:58:50 -0500

    login working

--- main.js
+++ main.js
@@ -20,14 +20,14 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.session({ secret: 'tenthousandapp' }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 var achievementController = new AchievementController('localhost', 27017);
 var userController = new UserController('localhost', 27017);
@@ -114,8 +114,8 @@ app.post('/user', function(req, res) {
 });
 
 app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/#login',
+  passport.authenticate('local', { successRedirect: '/achievements',
+                                   failureRedirect: '/login',
                                    failureFlash: false })
 );
 
