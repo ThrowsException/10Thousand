27a3759793d7766c05dc180f24711a2ac7b046c9
Chester ONeill <c.oneill.codes@gmail.com>
2013-12-10 13:58:03 -0500

    chart rendering working

--- routes/achievements.js
+++ routes/achievements.js
@@ -7,25 +7,25 @@ var AchievementController = require('../AchievementController').AchievementContr
 var achievementController = new AchievementController('localhost', 27017);
 
 exports.list = function(req, res){
-  achievementController.findAll(req.user._id, function(error, docs) {
-    res.render('achievements', { achievements : docs });
+  achievementController.findAll(req.user._id, function(error, data) {
+    res.render('achievements', { achievements : data });
   });
 };
 
 exports.detail = function(req, res){
-  achievementController.findById(req.params.id, function(error, docs) {
+  achievementController.findById(req.params.id, function(error, data) {
     //lets keep the updates array in order by date
-    if(docs && docs.updates){
-      docs.updates.sort(function(a, b) {
+    if(data && data.updates){
+      data.updates.sort(function(a, b) {
         //guard against nulls or undefined in the array
         if(a && b) {
           return a[0] - b[0];
         }
       });
-      res.render('achievement', { updates : docs });
+      res.render('achievement', { achievement : data });
     }
     else {
-      es.render('achievement', { updates : docs });
+      es.render('achievement', { achievement : data });
     }
   });
 };
