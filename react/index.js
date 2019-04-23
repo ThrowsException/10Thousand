c4050fa11ed11c7704a3d7f2014e306077f390e7
Chester ONeill <coneillcodes@gmail.com>
2019-04-23 00:14:24 -0400

    Function component all the things

--- react/index.js
+++ react/index.js
@@ -2,22 +2,14 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { ResponsiveLine, Line } from "@nivo/line";
 
-class Welcome extends React.Component {
-  componentDidMount() {
-    // fetch("achievements").then(function(response) {
-    //   console.log(response);
-    // });
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Hello, {this.props.name}</h1>
-        <Chart />
-      </>
-    );
-  }
-}
+const Welcome = props => {
+  return (
+    <>
+      <h1>Hello, {props.name}</h1>
+      <Chart />
+    </>
+  );
+};
 
 const TrackingForm = ({ submit }) => {
   const [date, setDate] = useState(new Date());
@@ -42,52 +34,37 @@ const TrackingForm = ({ submit }) => {
   );
 };
 
-class Chart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { data: [] };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Chart = () => {
+  const [data, setData] = useState([]);
 
-  handleClick(date, hours) {
+  const handleClick = (date, hours) => {
     const newElement = { x: date, y: hours };
 
-    this.setState(prevState => {
-      if (prevState.data.length == 0) {
-        return { data: [newElement] };
-      }
-
+    if (data.length === 0) {
+      setData([newElement]);
+    } else {
       // find if date already exists
-      const updateExisting = prevState.data.find(o => o.x === date);
+      const updateExisting = data.find(o => o.x === date);
 
       // Find item to update
       if (updateExisting) {
-        let data = prevState.data.map(el => {
+        let updated = data.map(el => {
           if (el.x == date) {
             return Object.assign({}, el, { y: hours });
           } else return el;
         });
-        return { data };
+        setData(updated);
+      } else {
+        setData([...data, { x: date, y: hours }]);
       }
+    }
+  };
 
-      return {
-        data: [...prevState.data, { x: date, y: hours }]
-      };
-    });
-  }
-
-  render() {
-    const data = [
-      {
-        id: "achievement",
-        data: this.state.data
-      }
-    ];
-
-    const MyResponsiveLine = ({ data }) => (
+  const MyResponsiveLine = ({ achievement }) => {
+    return (
       <ResponsiveLine
         margin={{ top: 20, right: 20, bottom: 60, left: 80 }}
-        data={data}
+        data={achievement}
         xScale={{
           type: "time",
           format: "%Y-%m-%d",
@@ -98,14 +75,21 @@ class Chart extends React.Component {
         }}
       />
     );
+  };
 
-    return (
-      <div style={{ height: "600px", width: "900px" }}>
-        <MyResponsiveLine data={data} />
-        <TrackingForm submit={this.handleClick} />
-      </div>
-    );
-  }
-}
+  return (
+    <div style={{ height: "600px", width: "900px" }}>
+      <MyResponsiveLine
+        achievement={[
+          {
+            id: "achievement",
+            data: data
+          }
+        ]}
+      />
+      <TrackingForm submit={handleClick} />
+    </div>
+  );
+};
 
 ReactDOM.render(<Welcome />, document.getElementById("root"));
