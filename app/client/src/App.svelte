b671fb55ab6b3a5526609c271bd0b285acbf13cc
Chester ONeill <coneillcodes@gmail.com>
2020-05-24 13:43:39 -0400

    Code Cleanup

--- app/client/src/App.svelte
+++ app/client/src/App.svelte
@@ -21,25 +21,25 @@
   export const handleClick = () => {
     data.push({ date: new Date(date), value: hours });
     data.sort((a, b) => a.date - b.date);
-    addData(data);
+    addData();
   };
 
   const margin = { top: 20, right: 30, bottom: 30, left: 40 };
   const width = 1000;
   const height = 200;
 
-  const y = d3
+  let y = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d.value)])
     .nice()
     .range([height - margin.bottom, margin.top]);
 
-  const x = d3
+  let x = d3
     .scaleUtc()
     .domain(d3.extent(data, d => d.date))
     .range([margin.left, width - margin.right]);
 
-  const line = d3
+  let line = d3
     .line()
     .defined(d => !isNaN(d.value))
     .x(d => x(d.date))
@@ -94,50 +94,30 @@
     d3.select(el).append(() => svg.node());
   };
 
-  const addData = data => {
-    const y = d3
+  const addData = () => {
+    y = d3
       .scaleLinear()
       .domain([0, d3.max(data, d => d.value)])
       .nice()
       .range([height - margin.bottom, margin.top]);
 
-    const x = d3
+    x = d3
       .scaleUtc()
       .domain(d3.extent(data, d => d.date))
       .range([margin.left, width - margin.right]);
 
-    const line = d3
-      .line()
-      .defined(d => !isNaN(d.value))
-      .x(d => x(d.date))
-      .y(d => y(d.value));
-
-    const yAxis = g =>
-      g
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y))
-        .call(g => g.select(".domain").remove())
-        .call(g =>
-          g
-            .select(".tick:last-of-type text")
-            .clone()
-            .attr("x", 3)
-            .attr("text-anchor", "start")
-            .text(data.y)
-        );
-
-    const xAxis = g =>
-      g.attr("transform", `translate(0,${height - margin.bottom})`).call(
-        d3
-          .axisBottom(x)
-          .ticks(width / 80)
-          .tickSizeOuter(0)
-      );
-
     const svg = d3.select(el);
-    svg.selectAll("g.xaxis").call(xAxis);
-    svg.selectAll("g.yaxis").call(yAxis);
     svg.select(".line").attr("d", line);
+    svg
+      .selectAll("g.xaxis")
+      .transition()
+      .duration(1500)
+      .call(xAxis);
+    svg
+      .selectAll("g.yaxis")
+      .transition()
+      .duration(1500)
+      .call(yAxis);
   };
 </script>
 
