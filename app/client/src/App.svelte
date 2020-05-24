7d89acdc8831e25ef9ab5367d5eb296b9e2d8368
Chester ONeill <coneillcodes@gmail.com>
2020-05-24 12:55:06 -0400

    Just redeclare everything to update it until I figure out a better way

--- app/client/src/App.svelte
+++ app/client/src/App.svelte
@@ -20,7 +20,8 @@
 
   export const handleClick = () => {
     data.push({ date: new Date(date), value: hours });
-    addData();
+    data.sort((a, b) => a.date - b.date);
+    addData(data);
   };
 
   const margin = { top: 20, right: 30, bottom: 30, left: 40 };
@@ -71,12 +72,12 @@
 
     svg
       .append("g")
-      .attr("class", "x axis")
+      .attr("class", "xaxis")
       .call(xAxis);
 
     svg
       .append("g")
-      .attr("class", "y axis")
+      .attr("class", "yaxis")
       .call(yAxis);
 
     svg
@@ -94,10 +95,49 @@
   };
 
   const addData = data => {
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.value)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    const x = d3
+      .scaleUtc()
+      .domain(d3.extent(data, d => d.date))
+      .range([margin.left, width - margin.right]);
+
+    const line = d3
+      .line()
+      .defined(d => !isNaN(d.value))
+      .x(d => x(d.date))
+      .y(d => y(d.value));
+
+    const yAxis = g =>
+      g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select(".domain").remove())
+        .call(g =>
+          g
+            .select(".tick:last-of-type text")
+            .clone()
+            .attr("x", 3)
+            .attr("text-anchor", "start")
+            .text(data.y)
+        );
+
+    const xAxis = g =>
+      g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+        d3
+          .axisBottom(x)
+          .ticks(width / 80)
+          .tickSizeOuter(0)
+      );
+
     const svg = d3.select(el);
+    svg.selectAll("g.xaxis").call(xAxis);
+    svg.selectAll("g.yaxis").call(yAxis);
     svg.select(".line").attr("d", line);
-    svg.select(".x axis").call(xAxis);
-    svg.select(".y axis").call(yAxis);
   };
 </script>
 
